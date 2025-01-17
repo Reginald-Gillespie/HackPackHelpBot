2ab12f6c144636745411b409ce8d994772fb7280
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-01-17 10:16:33 -0600

    JSON chart renderer for development

--- Flowcharts/README.md
+++ Flowcharts/README.md
@@ -1,3 +1,5 @@
-The template.html in this folder is loaded an injected with the requested chart, parsed into mermaid syntax. The chart hex theme color is extracted from a comment and in injected into the template as well. 
+The `render.js` script renders all `.json` files into images, stored in the cache folder.
 
-The cache folder stores the images to a template by hashing the resulting html file (it takes a few seconds to generate flowcharts).
+Any `.json` files in this folder are parsed into `mermaid` chart syntax, then injected into the `template.html` in this folder. requested chart, parsed into mermaid syntax. The chart hex theme color is extracted from a comment and in injected into the template as well.
+
+The cache folder stores the rendered webpage, named by the hash of the webpage injected template (to allow easy cache checking).
---
+++ Flowcharts/render.js
@@ -0,0 +1,43 @@
+// Developer script to render all .JSON flowcharts into the ./cache directory
+
+const fs = require('fs');
+
+// cd to this folder
+const path = require('path');
+const process = require('process');
+process.chdir(path.dirname(__filename));
+
+(async function main() {
+
+    // Import renderer
+    const { getChartOptions, getPathToFlowchart } = require("../flowcharter")
+    const allCharts = getChartOptions();
+    const usedCache = []; // Keep track of unused cached and remove it to keep things clean
+    for (chart of allCharts) {
+        console.log(`\n${"=".repeat(50)}\nRendering ${chart}`);
+        const [imagePath, error] = await getPathToFlowchart(chart, false);;
+        if (error) {
+            console.error(`\x1b[31m${error}\x1b[0m`);
+            continue;
+        }
+        // Copy image to more readable path
+        const newCharPath = `./Flowcharts/cache/${chart}-dev.jpg`;
+        fs.copyFileSync(imagePath, newCharPath);
+        console.log(`\x1b[32mSaved to ${newCharPath}\x1b[0m`);
+
+        usedCache.push(newCharPath);
+        usedCache.push(imagePath);
+    }
+
+    // Remove unused cache
+    const cacheDir = "./Flowcharts/cache";
+    const files = fs.readdirSync(cacheDir);
+    files.forEach(file => {
+        if (!usedCache.includes(`${cacheDir}/${file}`)) {
+            fs.unlinkSync(`${cacheDir}/${file}`);
+        }
+    })
+
+    console.log(`\n${"=".repeat(50)}\nFinished`);
+
+})();
