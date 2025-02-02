b09f078818bbb3f72b99d1b3a906dad3652330a3
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-02-02 21:34:57 +0000

    Latest running code

--- index.js
+++ index.js
@@ -785,7 +785,7 @@ client.on('messageCreate', async (message) => {
         storage.dupeNotifs &&
         existingQuestion.guildId == message.guildId &&
         existingQuestion.repeats > 1 &&
-        normalizedContent.length > 23 && // Required flags
+        normalizedContent.length > 30 && // Required flags
         ( // Must contain one of these:
             /\?/.test(normalizedContent) ||
             /anyone know/.test(normalizedContent) ||
@@ -797,8 +797,7 @@ client.on('messageCreate', async (message) => {
             /isnt/.test(normalizedContent) ||
             /is not/.test(normalizedContent) ||
             /it was/.test(normalizedContent) ||
-            /help/.test(normalizedContent) ||
-            /what/.test(normalizedContent)
+            /help/.test(normalizedContent)
         )
     ) {
         try {
