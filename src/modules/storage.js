47a6f6323e48ae22bc04d3f022d0a4e9167cf093
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-04-10 23:48:23 -0500

    Better saving method

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,28 +1,30 @@
 // Helper module to manage database saving
 
 // This method uses json, which is small and easy to work with.
-// Two storage files are written to back and forth, to avoid corrupting the DB if it crashes while writing.
+// Write to a temp file and then rename to avoid corrupting the DB if it crashes while writing.
 
 const fs = require("fs");
 const path = require("path")
 
 class Storage {
     constructor() {
-        this.privStorageLocations = [
-            path.join(__dirname, "../../Storage/storage1.json"),
-            path.join(__dirname, "../../Storage/storage2.json")
-        ];
-        this.privStorageCycleIndex = 0; // cycle writes between files
-        this.data = this.readLatestDatabase(this.privStorageLocations);
+        const baseDir = path.join(__dirname, "../../Storage");
+
+        this.privStorageLocations = {
+            current: path.join(baseDir, "storage.json"),
+            temp: path.join(baseDir, "storage-temp.json"),
+            backup: path.join(baseDir, "storage-bak.json")
+        };
+        this.data = this.readLatestDatabase([this.privStorageLocations.current, this.privStorageLocations.backup]);
         this.cache = {}; // a temporary global version of storage for convenience
 
-        // Proxy to save help messages on write 
-        this.helpMessageLocations = [
-            path.join(__dirname, "../../Storage/helpMessagesSave1.json"),
-            path.join(__dirname, "../../Storage/helpMessagesSave2.json")
-        ];
-        this.helpMessageCycleIndex = 0; // cycle writes between files
-        const _helpMessages = this.readLatestDatabase(this.helpMessageLocations);
+        // Proxy to save help messages on write
+        this.helpMessageLocations = {
+            current: path.join(baseDir, "helpMessagesSave.json"),
+            temp: path.join(baseDir, "helpMessagesSave-temp.json"),
+            backup: path.join(baseDir, "helpMessagesSave-bak.json")
+        };
+        const _helpMessages = this.readLatestDatabase([this.helpMessageLocations.current, this.helpMessageLocations.backup]);
         this.helpMessages = _helpMessages; // These are saved by calling saveHelps directly instead of by the proxy
 
         // Return a proxy reference to this object so we can do storageObj[key] storageObj.data[key]
@@ -46,11 +48,11 @@ class Storage {
                 return true;
             }
         });
-        
+
     }
 
     readLatestDatabase(storageLocs) {
-    
+
         // We'll overwrite these right away once we read the correct one
         const corruptedFiles = []
 
@@ -63,7 +65,7 @@ class Storage {
             }))
             .sort((a, b) => b.mtime - a.mtime)  // sort this array by the most frequent
             .map(({ file }) => file);
-        
+
         for (let location of sortedLocations) {
             try {
                 const data = require(location);
@@ -74,7 +76,7 @@ class Storage {
                     console.log(`Fixing corrupted file at ${file}`)
                     fs.writeFileSync(file, JSON.stringify(data));
                 })
-                
+
                 return data;
             } catch (e) {
                 corruptedFiles.push(location)
@@ -84,22 +86,36 @@ class Storage {
 
         if (corruptedFiles.length == 0) return {} // storage does not exist, create empty
 
-        // This case should never be hit - in theory we could try to load from the latest google drive. 
+        // This case should never be hit - in theory we could try to load from the latest google drive.
         console.log(`No storage locations could be loaded. Tried: ${sortedLocations.join(", ")}.`)
         process.exit();
     }
 
     savePrivStorage() {
-        let writeLocation = this.privStorageLocations[this.privStorageCycleIndex % this.privStorageLocations.length];
-        fs.writeFileSync(writeLocation, JSON.stringify(this.data, null, 4));
-        this.privStorageCycleIndex++; 
-        // console.log(`Just wrote DB to ${writeLocation}`)
+        const tempLocation = this.privStorageLocations.temp;
+        const currentLocation = this.privStorageLocations.current;
+        const backupLocation = this.privStorageLocations.backup;
+
+        fs.writeFileSync(tempLocation, JSON.stringify(this.data, null, 4));
+
+        if (fs.existsSync(currentLocation)) {
+            fs.renameSync(currentLocation, backupLocation);
+        }
+        fs.renameSync(tempLocation, currentLocation);
+        // console.log(`Just wrote DB to ${currentLocation}`)
     }
 
     saveHelps() {
-        let writeLocation = this.helpMessageLocations[this.helpMessageCycleIndex % this.helpMessageLocations.length];
-        fs.writeFileSync(writeLocation, JSON.stringify(this.helpMessages, null, 4));
-        this.helpMessageCycleIndex++; 
+        const tempLocation = this.helpMessageLocations.temp;
+        const currentLocation = this.helpMessageLocations.current;
+        const backupLocation = this.helpMessageLocations.backup;
+
+        fs.writeFileSync(tempLocation, JSON.stringify(this.helpMessages, null, 4));
+
+        if (fs.existsSync(currentLocation)) {
+            fs.renameSync(currentLocation, backupLocation);
+        }
+        fs.renameSync(tempLocation, currentLocation);
     }
 }
 
