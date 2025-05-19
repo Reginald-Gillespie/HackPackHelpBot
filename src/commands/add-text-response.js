60ac6480047ffcc53500b24fb65f8689d50fe0ac
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-05-19 14:59:12 -0500

    Current progress to move to mongo server

---
+++ src/commands/add-text-response.js
@@ -0,0 +1,55 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+} = require('discord.js');
+const { CustomResponses } = require("../modules/database")
+
+module.exports = {
+    data: new SlashCommandBuilder().setName("add-text-response").setDescription("Create a custom response to text commands like !daily")
+        .addStringOption(option =>
+            option.setName("trigger").setDescription("The text the message must start with to trigger").setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName("response").setDescription("The text to respond with").setAutocomplete(true).setRequired(false)
+        )
+        .addBooleanOption(option =>
+            option.setName("delete").setDescription("Whether to delete this option").setRequired(false)
+        ),
+
+    async execute(cmd) {
+        const trigger = cmd.options.getString("trigger");
+        const response = cmd.options.getString("response");
+        const shouldDelete = cmd.options.getBoolean("delete");
+
+        if (shouldDelete) {
+            // Remove this one
+            const result = await CustomResponses.deleteOne({
+                trigger: trigger
+            })
+            
+            if (result.deletedCount) {
+                await cmd.reply(`Removed custom response.`);
+            } 
+            else {
+                await cmd.reply({
+                    content: `No custom response found for that trigger`,
+                    ephemeral: true
+                });
+            }
+            return;
+        }
+
+        if (!response) {
+            await cmd.reply("You must provide a response unless deleting.");
+            return;
+        }
+
+        await CustomResponses.updateOne(
+            { trigger: trigger },
+            { $set: { response: response } },
+            { upsert: true }
+        );
+
+        await cmd.reply(`Added custom response for trigger: \`${trigger}\``);
+    }
+}
